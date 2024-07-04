c0845f4dfaae859a881e1e82b94b6f77f4734cbc
lee <80160069+MulBang@users.noreply.github.com>
2024-07-04 13:41:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const largeCloud = document.querySelector('.large');
-  const mediumCloud = document.querySelector('.medium');
+  const greeting = document.getElementById('greeting');
+  const subGreeting = document.getElementById('subGreeting');
+  const resume = document.querySelector('.resume');
   
-  largeCloud.addEventListener('animationend', () => {
-    mediumCloud.style.animationPlayState = 'running';
-  });
-
-  mediumCloud.addEventListener('animationend', () => {
-    document.querySelector('.resume').style.display = 'block';
-  });
+  // "안녕하세요" 나타나기
+  setTimeout(() => {
+    greeting.style.opacity = 1;
+  }, 500); // 0.5초 후
+  
+  // "이수빈의 포트폴리오입니다. 환영합니다!" 나타나기
+  setTimeout(() => {
+    subGreeting.style.opacity = 1;
+  }, 1200); // 0.2초 후 추가 0.5초
+  
+  // Resume 창 나타나기
+  setTimeout(() => {
+    resume.style.display = 'block';
+  }, 1700); // 추가 0.5초
 });
