5db090bd7e49681a6eb83bc824bf82ebd965e277
lee <80160069+MulBang@users.noreply.github.com>
2024-07-04 13:43:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ document.addEventListener("DOMContentLoaded", () => {
   // "이수빈의 포트폴리오입니다. 환영합니다!" 나타나기
   setTimeout(() => {
     subGreeting.style.opacity = 1;
-  }, 1200); // 0.2초 후 추가 0.5초
+  }, 1200); // 0.7초 후
   
   // Resume 창 나타나기
   setTimeout(() => {
     resume.style.display = 'block';
-  }, 1700); // 추가 0.5초
+  }, 1700); // 1.2초 후
 });
